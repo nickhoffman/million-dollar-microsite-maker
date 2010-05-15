f3ecfe49ce50082182675ccd3361c16594de40eb
Andrew Louis <andrew@hyfen.net>
2010-05-15 15:16:43 -0400

    Basic listing of CanPages results into the microsite view

--- app/controllers/microsites_controller.rb
+++ app/controllers/microsites_controller.rb
@@ -3,6 +3,10 @@ class MicrositesController < ApplicationController
   layout "microsite"
   
   def generate
+    @companies = CanPages.search :loc => params[:location], :key => params[:topic]
+    
+    # debugger
+    
     @location = "Toronto"
     @topic = "Dating"
   end
